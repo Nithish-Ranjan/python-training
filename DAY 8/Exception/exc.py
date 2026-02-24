dff1f25dac6cf92ddaa421c41b91b60ca4ad8339
NITHISH RANJAN <nithishranjanjumnalmath@gmail.com>
2026-02-24 13:19:01 +0530

    Add exception handling examples in exc.py

---
+++ DAY 8/Exception/exc.py
@@ -0,0 +1,29 @@
+# Exception
+
+# a = int(input("enter the number:"))
+# try:
+#     print(10/a)
+    
+# except ZeroDivisionError:
+#     print("sorry you cannot divide by 0")
+# else:
+#     print("there is no exception")
+    
+# finally:
+#     print("i will run no matter what")
+    
+# print("program is executed")
+
+# l=[1,2,3,4]
+# print(l(6))
+
+# print("10"+5)
+
+# age = int(input("enter your age:"))
+# if age<10 or age>18:
+#     raise ValueError("your age must be between 10 and 18")
+# else:
+#     print("welcome to the club")
+
+# except Exception as e:
+#     print(f"an error occured as {e}")
