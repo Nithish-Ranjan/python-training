6e4455924f533c931f05fbdeb0a15a1dac5711bb
NITHISH RANJAN <nithishranjanjumnalmath@gmail.com>
2026-02-26 13:03:40 +0530

    Refactor non-zero element positioning logic in non0.py

--- DAY 10/non0.py
+++ DAY 10/non0.py
@@ -1,10 +1,12 @@
 # Put all non 0 elements at last
 
 l = [1,0,3,0,6]
-
-for i in range(len(l)-1):
-    if l[i] == 0:
-        l.remove(l[i])
-        l.append(0)
+j = 0
+for i in range(len(l)):
+    if l[i] != 0:
+        l[i],l[j] = l[j],l[i]
+        j += 1
+        # l.remove(l[i])
+        # l.append(0)
         
 print(l)
